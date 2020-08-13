599422c9e5241fd3e2babd87be49edc9e081625e
Andrew Addie <theaddies@neo.rr.com>
2020-08-13 13:40:30 -0400

    filtering

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -13,3 +13,35 @@ data.forEach((sighting) => {
         cell.text(value);
     });
 });
+
+var button = d3.select("#filter-btn");
+
+var form = d3.select("#datetime");
+
+// create event handlers
+
+button.on("click", runEnter);
+form.on("submit", runEnter);
+
+function runEnter(){
+    d3.event.preventDefault();
+    var inputElement = d3.select("#datetime");
+
+    var inputValue = inputElement.property("value");
+    console.log(inputValue)
+
+    var filteredData = tableData.filter(row => row.datetime === inputValue);
+
+    //this is where we need to clear the table
+    
+    d3.selectAll("tr").remove()
+
+    var tbody = d3.select("tbody");
+    filteredData.forEach((sighting) => {
+        var row = tbody.append("tr").style("color", "white");
+        Object.entries(sighting).forEach(([key, value]) => {
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
+}
