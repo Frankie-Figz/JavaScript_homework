8abb52f7f042a4ee30d981d0525558191d78f18d
Andrew Addie <theaddies@neo.rr.com>
2020-08-13 21:33:11 -0400

    working on UFO-level-3

--- UFO-level-3/static/js/app.js
+++ UFO-level-3/static/js/app.js
@@ -8,7 +8,7 @@ var tbody = d3.select("tbody");
 
 data.forEach((sighting) => {
     var row = tbody.append("tr").style("color", "white");
-    Object.entries(sighting).forEach(([key, value]) => {
+    Object.values(sighting).forEach((value) => {
         var cell = row.append("td");
         cell.text(value);
     });
@@ -25,18 +25,33 @@ form.on("submit", runEnter);
 
 function runEnter(){
     d3.event.preventDefault();
-    var inputElement = d3.select("#datetime");
+    var dateValue = d3.select("#datetime").property("value");
+    var cityValue = d3.select("#city").property("value");
+    var stateValue = d3.select("#state").property("value");
+    var countryValue = d3.select("#country").property("value");
+    var shapeValue = d3.select("#shape").property("value");
 
-    var inputValue = inputElement.property("value");
-    console.log(inputValue)
+    console.log(dateValue);
+    console.log(cityValue);
+    console.log(stateValue);
+    console.log(countryValue);
+    console.log(shapeValue);
 
-    var filteredData = tableData.filter(row => row.datetime === inputValue);
+    var filteredData = tableData.filter(row => {
+        if((row.datetime == dateValue || dateValue == "") &&
+            (row.city == cityValue || cityValue == "") &&
+            (row.state == stateValue || stateValue == "") &&
+            (row.country == countryValue || countryValue == "") &&
+            (row.shape == shapeValue || shapeValue == "")) {
+            return true}
+    });
 
     //this is where we need to clear the table
     
     d3.select("tbody").selectAll("tr").remove()
 
     var tbody = d3.select("tbody");
+
     filteredData.forEach((sighting) => {
         var row = tbody.append("tr").style("color", "white");
         Object.entries(sighting).forEach(([key, value]) => {
