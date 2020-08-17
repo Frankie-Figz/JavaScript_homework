20d4e5d1b33eef1dfb884b057f1c6fc2dd155af4
Andrew Addie <theaddies@neo.rr.com>
2020-08-17 08:19:11 -0400

    city list working

--- UFO-level-3/static/js/app.js
+++ UFO-level-3/static/js/app.js
@@ -1,27 +1,56 @@
 // from data.js
 var tableData = data;
+console.log(tableData);
+console.log(tableData[0].city);
+var cityList = tableData.map(function(row){
+    return row.city
+})
 
+var stateList = tableData.map(function(row){
+    return row.state
+})
+
+var uniqueCity = Array.from(new Set(cityList));
+var uniqueState = Array.from(new Set(stateList));
+console.log(uniqueCity);
+console.log(uniqueState);
 // YOUR CODE HERE!
 //select tbody the location to add the table
 
 var tbody = d3.select("tbody");
 
-data.forEach((sighting) => {
-    var row = tbody.append("tr").style("color", "white");
-    Object.values(sighting).forEach((value) => {
-        var cell = row.append("td");
-        cell.text(value);
-    });
-});
+var selectCity = d3.select("#city").node();
+
+var cityValue = selectCity.value
+console.log("here");
+console.log(selectCity);
+console.log("here1");
+console.log(d3.select("#city").selectAll("option"))
+updateCityList(uniqueCity);
+
+function updateCityList(uniqueCity){
+    //d3.event.preventDefault();
+    console.log(d3.select("#city"))
+    d3.select("#city").selectAll("option").remove()
+    uniqueCity.forEach(function(city){
+        var row = d3.select("#city");
+        row.append("option").text(city);
+        var test = row.select("option")
+        console.log(test)
+    })
+}
 
 var button = d3.select("#filter-btn");
 
 var form = d3.select("#datetime");
 
+var cityFilter = d3.select('#city')
+
 // create event handlers
 
 button.on("click", runEnter);
 form.on("submit", runEnter);
+cityFilter.on("change",runEnter )
 
 function runEnter(){
     d3.event.preventDefault();
